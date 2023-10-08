1c3d7ae4fdfc869e5500e837835127ee3e567c4f
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2023-10-08 17:17:30 +0900

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function compSass() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({ 
+            outputStyle: 'compressed'
+        }))
+        .pipe(gulp.dest('./dist/css'));
+}
+
+exports.default = gulp.parallel(compSass);
+
+exports.watch = function () {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(compSass));
+}
